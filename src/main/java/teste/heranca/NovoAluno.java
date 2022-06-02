1f3466488dbdb3da364ff40f7173b47cf8100f9d
marciliodevjava <marciliodevjava@gmail.com>
2022-06-02 07:34:00 -0300

    Atualização

---
+++ src/main/java/teste/heranca/NovoAluno.java
@@ -0,0 +1,20 @@
+package teste.heranca;
+
+import infra.DAO;
+import modelo.heranca.Aluno;
+import modelo.heranca.AlunoBolsista;
+
+public class NovoAluno {
+
+	public static void main(String[] args) {
+		
+		DAO<Aluno> alunoDao = new DAO<>();
+		Aluno aluno1 = new Aluno(123L, "João");
+		AlunoBolsista aluno2 = new AlunoBolsista(234L, "Maria", 1000);
+		
+		alunoDao.incluirAtomico(aluno1);
+		alunoDao.incluirAtomico(aluno2);
+		
+		alunoDao.fechar();
+	}
+}
